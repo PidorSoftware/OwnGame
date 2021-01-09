a76ec2a75004d17c24327d4916b4b62663ed55ae
StepOne <dobruyyoba@gmail.com>
2021-01-09 11:33:09 +0300

    Not Finaly Version

--- client/app.py
+++ client/app.py
@@ -1,6 +1,6 @@
 import kivy
 import random
-import time
+from time import sleep
 
 from kivy.app import App
 from kivy.graphics import Color, Rectangle
@@ -14,36 +14,45 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
 from kivy.clock import Clock
-used_questions = []
+import inspect
 
-def my_callback():
-    pass
+
+
+string = """Блять, да мне похуй на тебя, блять, слушай, какая у тебя там тачка, блять, плюсы, джавы там блять, 
+си, всё, мне похуй, хоть там "Руби", хоть блять нахуй "Котлин", хоть "Шарпы", хоть "Делфи" блять, 
+хоть стометровый баш, мне на это насрать, понимаешь?"""
 
 
 class MyApp(App):	
 
+	def my_callback(self, dt):
+	    pass
+
 	def build(self):
+		parser = Button()
+		print(inspect.getmembers(parser, predicate=inspect.ismethod))
 		Window.clearcolor = (0, 0.02745098, 0.525490196, 1)
 		self.gl = GridLayout(cols = 5, rows = 5, padding = 100, spacing = 10, )
 		for i in range(5):
 			for j in range(5):
-				if([i,j] not in used_questions):
-					self.gl.add_widget( Button(text = str((j + 1) * 100),
+				self.gl.add_widget( Button(text = str((j + 1) * 100),
 										background_color = (0, 0, 1, 1),
 										on_press = self.show_question,
-										background_disabled_down = '' ) )
-		'''else:
-					self.gl.add_widget( Button(background_color = (0, 0.02745098, 0.525490196, 1),
-										background_normal = '') )'''
+										background_disabled_down = '' ), index = i)
 		return self.gl
+
+	def dismiss_popup(self, dt):
+		self.popup.dismiss()
 
 	def show_question(self, instance):
 		if instance.text:
+			self.popup = Popup(title='question', content=Label(text=string),
+			              auto_dismiss=False)
+			self.popup.open()
 			instance.text = ''
 			instance.background_color = (1, 1, 1, 0)
-			popup = Popup(title='Test popup', content=Label(text='Hello world'),
-			              auto_dismiss=0.0000001)
-			popup.open()
+			Clock.schedule_once(self.dismiss_popup, 5)
+			
 
 if __name__ == '__main__':
 	MyApp().run()
