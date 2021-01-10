dc66e5ed0bdb274c79db8825b1637f15b63cae52
StepOne <dobruyyoba@gmail.com>
2021-01-10 07:46:14 +0300

    fixed

--- client/app.py
+++ client/app.py
@@ -6,8 +6,10 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
 from kivy.uix.popup import Popup
 from kivy.clock import Clock
+from kivy.uix.label import Label
 
-background_color = (0, 0.02745098, 0.525490196, 1)
+
+background_color = (0.0, 0.02745098, 0.525490196, 1.0)
 
 string = """Блять, да мне похуй на тебя, блять, слушай, какая у тебя там тачка, блять, плюсы, джавы там блять, 
 си, всё, мне похуй, хоть там "Руби", хоть блять нахуй "Котлин", хоть "Шарпы", хоть "Делфи" блять, 
@@ -19,10 +21,10 @@ class MyApp(App):
 		Window.clearcolor = background_color
 		self.gl = GridLayout(cols = 5, rows = 5, padding = 100, spacing = 10)
 		for i in range(25):
-			self.gl.add_widget( Button(text = str(((j % 5) + 1) * 100),
-									background_color = (0, 0, 1, 1),
+			self.gl.add_widget( Button(text = str(((i % 5) + 1) * 100),
+									background_color = (0.0, 0.0, 1.0, 1.0),
 									on_press = self.show_question,
-									background_disabled_down = '' ), index = i)
+									background_disabled_down = '' ) )
 		return self.gl
 
 	def dismiss_popup(self, dt):
@@ -30,11 +32,10 @@ class MyApp(App):
 
 	def show_question(self, instance):
 		if instance.text:
+			instance.clear_widgets()
 			self.popup = Popup(title='question', content=Label(text=string), auto_dismiss=False)
 			self.popup.open()
-			instance.clear_widgets()
-			Clock.schedule_once(self.dismiss_popup, 5)			
+			Clock.schedule_once(self.dismiss_popup, 1)			
 
 if __name__ == '__main__':
 	MyApp().run()
-
